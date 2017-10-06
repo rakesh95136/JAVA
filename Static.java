d63cb4747fcc8ed06d908a64bddd1b04e8c55ec7
rakesh95136 <rakesh95136@yahoo.com>
2017-10-06 08:44:10 -0700

    messages

---
+++ Static.java
@@ -0,0 +1,34 @@
+public class Static
+{
+	
+	int y = 2;
+	static int i = 2;
+	static int t = 99;
+
+	public static void fun1()
+	{
+				
+	}
+
+	public static void fun2()
+	{
+		
+	}
+
+
+	public static class A
+	{
+
+	}
+
+	public static class B
+	{
+
+	}
+
+	public static void main(String[] arg)
+	{
+		System.out.println("Hello");
+	}
+
+}
