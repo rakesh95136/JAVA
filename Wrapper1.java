a7ba86a0ec8756ffc07b08771e571f43b73f2d9f
rakesh95136 <rakesh95136@yahoo.com>
2017-10-06 09:11:19 -0700

    messages

---
+++ Wrapper1.java
@@ -0,0 +1,14 @@
+public class Wrapper1
+{
+
+	public static void main(String[] args)
+	{
+		
+		Integer i1 = Integer.valueOf("123");
+		Integer i2 = Integer.valueOf("1010",2); // This is in binary form and print 10
+		Double d1 = Double.valueOf("3.14");
+		System.out.println(i1);  //Print 123
+		System.out.println(i2);
+	}
+
+}
